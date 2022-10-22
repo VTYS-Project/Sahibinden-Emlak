584ffaa489c5e4abcfb1a0fd75f642b9b890754f
firatarikkan <firat.arikkan@gmail.com>
2022-10-22 16:14:01 +0300

    layout extends

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="TR-tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sahibinden Emlak | @yield("title")</title>
+    @yield("css")
+</head>
+<body>
+    
+@yield("main")
+
+@yield("js")
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Sahibinden Emlak</title>
-    </head>
-    <body>
-       sahibinden emlak home page
-    </body>
-</html>
+@extends("layout.layout")
+@section("title")
+HOME
+@endsection
