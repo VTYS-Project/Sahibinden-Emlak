22cd4e1d87ef8156495455e56e2cc9c0d3d524ad
firatarikkan <firat.arikkan@gmail.com>
2022-11-05 20:08:56 +0300

    anasayfa baş kısım belirsiz tamam

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sahibinden Emlak | @yield("title")</title>
-    @yield("css")
-</head>
-<body>
-    
-@yield("main")
-
-@yield("js")
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sahibinden Emlak | @yield("title")</title>
+        <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" ></script>
+        @yield("css")
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg nav">
+            <div class="container-fluid">
+                <a class="navbar-brand logo" href="/"><!--<img src="{{URL('/images/Logo.png')}}" width="120" height="70">-->BİZİM EMLAK</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end mt-3" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav_item-div_item" href="#">link</a>
+                        <a class="nav_item-div_item" href="#">link</a>   
+                        <a class="nav_item-div_item nav_item-div_item-2 justify-content-end" href="#">Giriş yap</a>
+                        <a class="nav_item-div_item nav_item-div_item-2 "  href="#">Kayıt ol</a>
+                    </div>
+                </div>
+            </div>
+            @yield("nav")
+        </nav>
+        @yield("main")
+        @yield("fotter")
+        @yield("js")
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,51 @@
 @extends("layout.layout")
+<!--    title   -->
 @section("title")
 HOME
+@endsection
+<!--    title end  -->
+
+<!--    css   -->
+@section("css")
+<link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+<style>
+    .section
+    {
+        position: absolute;
+        width: 80%;
+        height: 150px;
+        left: 10%;
+        top: 100%;
+
+background: #FFFFFF;
+border-radius: 50px;
+    }
+</style>
+@endsection
+<!--    css end  -->
+
+@section("main")
+    <section class="top_section">
+        <div class="main_div">
+            <img class="back_image" src="{{URL('images/Home_page_1.jpg')}}">
+            <div class="banner"></div>
+            <br><br><br>
+            <h1 class="nav_H1">
+                SİZE EV SATACAĞIZ
+            </h1>
+            <p class="nav_p">Doğru tercih her zaman karlıdır</p>
+        </div>
+        <div>
+            <form action="#" method="get" >
+            <input type="search" name="" id="" class="search">
+        </form>
+        </div>
+    </section>
+
+    <section class="section">
+        <div >hazırlanıyor . . .</div>
+        
+    </section>
+    
+    
 @endsection
